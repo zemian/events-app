03fce84257bba47f12a13f97a3248106021beef8
Zemian Deng <zemian.deng@oracle.com>
2020-03-16 13:07:08 -0400

    Update UI to use EventService

---
+++ src/services/EventService.js
@@ -0,0 +1,12 @@
+import axios from "axios"
+
+export default {
+    async getEvents() {
+        let res = await axios.get("http://localhost:8000/events");
+        return res.data;
+    },
+    async getEventSingle(eventId) {
+        let res = await axios.get("http://localhost:8000/events/" + eventId);
+        return res.data;
+    }
+}
--- src/views/EventSingle.vue
+++ src/views/EventSingle.vue
@@ -29,47 +29,30 @@
     </div>
 </template>
 <script>
+    // NEW - import EventService
+    import EventService from '@/services/EventService.js';
     export default {
         name: 'EventSingle',
-        data () {
+        data() {
+            // NEW - initialize the event object
             return {
-                events: [
-                    {
-                        id: 1,
-                        name: 'Charity Ball',
-                        category: 'Fundraising',
-                        description: 'Spend an elegant night of dinner and dancing with us as we raise money for our new rescue farm.',
-                        featuredImage: 'https://placekitten.com/500/500',
-                        images: [
-                            'https://placekitten.com/500/500',
-                            'https://placekitten.com/500/500',
-                            'https://placekitten.com/500/500',
-                        ],
-                        location: '1234 Fancy Ave',
-                        date: '12-25-2019',
-                        time: '11:30'
-                    },
-                    {
-                        id: 2,
-                        name: 'Rescue Center Goods Drive',
-                        category: 'Adoptions',
-                        description: 'Come to our donation drive to help us replenish our stock of pet food, toys, bedding, etc. We will have live bands, games, food trucks, and much more.',
-                        featuredImage: 'https://placekitten.com/500/500',
-                        images: [
-                            'https://placekitten.com/500/500'
-                        ],
-                        location: '1234 Dog Alley',
-                        date: '11-21-2019',
-                        time: '12:00'
-                    }
-                ],
                 event: {}
             }
         },
         created() {
-            const ID = Number(this.$route.params.id);
-            let event = this.events.find(event => event.id === ID);
-            this.event = event;
+            this.getEventData(); // NEW - call getEventData() when the instance is created
+        },
+        // NEW
+        methods: {
+            async getEventData() {
+                // Use the eventService to call the getEventSingle() method
+                EventService.getEventSingle(this.$route.params.id)
+                    .then(
+                        (event => {
+                            this.$set(this, "event", event);
+                        }).bind(this)
+                    );
+            }
         }
     }
 </script>
